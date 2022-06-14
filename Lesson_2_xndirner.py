653f21039eb09e9f2ba3515b481af33847f2f234
Karen-Mnacakanyan <107503251+Karen-Mnacakanyan@users.noreply.github.com>
2022-06-14 23:40:53 +0400

    Create Lesson_2_xndirner.py

---
+++ Lesson_2_xndirner.py
@@ -0,0 +1,21 @@
+#1 haytararel a ev b popoxakanner, tal int ev float arjeqner, bazmapatkel, pahpanel c-i mej, tpel c-n ev c- data type-y
+
+#2 haytararel a ev b popoxakanner, tal int ev float arjeqner, bazmapatkel, pahpanel c-i mej, tpel c-n ev c- data type-y
+
+#3 haytararel a ev b popoxakanner, tal str arjeq, pahpanel c-i mej ev tpel c-n u c-i data type-y
+
+#4 hatararel a ev b tarber tvayin popoxakanner, tvery gumarel irar pahpanel c-i mej, tpel` "c-n havasare e ai ev b-i gumarin", verjum tpel bolor popoxakanneri data type-ery
+
+#5 haytararel 2 str popoxakanner, pahpanel text popoxakani mej u tpel text popoxakany u data type-y
+
+#6 haytararel 3 tarber tvayin popoxakan , tpel data typery mi toxov
+
+#7 haytararel a int ev b str popoxakanner, tpel mi toxum
+
+#8 haytararel int ,float ev str poxakanner, tpel mi toxum
+
+#9 haytararel a int popoxakan, poxel data type-y float-i u pahpanel b-i mej, tpel b-i data type-y
+
+#10 haytararel a flaoat popoxakan, poxel data type-y int-i pahpanel b-i mej, tpel b-i data type-y
+
+# haytararel a int ev b float popoxakannery, poxel a-i ev b-i data typery str-i pahapanelov hamapatasxanabar x-i u y-i mej, tpel x-i u y-i data type-ery
