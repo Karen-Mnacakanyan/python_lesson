58e7707a8b05848147646dffdb609783b548deda
Karen-Mnacakanyan <107503251+Karen-Mnacakanyan@users.noreply.github.com>
2022-06-14 22:07:43 +0400

    Create python_lesson_2.py

---
+++ python_lesson_2.py
@@ -0,0 +1,28 @@
+"""
+===================================  Tiv  ================================
+integer codum ----------- int     -------- amboxj tvern en - minus anverjutyunic plus anvejutyun
+orinak` -10000000000, -567672, -23, -4, 0 , 1, 10,  1000, 943543593634
+
+
+float , codum ----------- float   -------- kotorakayin tver - minus anverjutyunic plus anvejutyun
+orinak `  -1.05 ,   -0.67886 , -18349433.32847 ,  0.23432,  0.1 , 1.34,  122.25 10000.56
+
+
+complex , codum --------- complex --------
+orinak `  x + 4,  2x + 5y, complex(2, 6)
+
+======================================= tox =====================================
+tox - string codum --------- str - comment chi
+orinak` "chakertnerum grvacy hamarvuma tox kap chuni tiva, tar te urish symbol, es bolory toxa ` !@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-="
+orinak 2 ` "es ela tox  "
+orinak 3 ` 'String, like this "3+5j",'
+
+
+
+zangvac (obeject)
+list
+set
+frozen set
+tuple
+dict
+"""
