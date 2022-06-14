8ade584ec4934c0b2cfd0f89e0a90f8caa08c4a9
Karen-Mnacakanyan <107503251+Karen-Mnacakanyan@users.noreply.github.com>
2022-06-14 23:19:32 +0400

    Update python_lesson_2.py

--- python_lesson_2.py
+++ python_lesson_2.py
@@ -10,23 +10,72 @@ orinak `  -1.05 ,   -0.67886 , -18349433.32847 ,  0.23432,  0.1 , 1.34,  122.25 
 
 complex , codum --------- complex --------
 orinak `  x + 4,  2x + 5y, complex(2, 6)
+===================================================================================
+
+
+
 
 ======================================= tox =====================================
 tox - string codum --------- str - comment chi
 orinak` "chakertnerum grvacy hamarvuma tox kap chuni tiva, tar te urish symbol, es bolory toxa ` !@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890-="
 orinak 2 ` "es ela tox  "
 orinak 3 ` 'String, like this "3+5j",'
+=================================================================================
 
 
 
+===================================================================
 zangvac (obeject)
 list
 set
 frozen set
 tuple
 dict
+========================================================================
 """
-
 #1. haytararel 3 tarber popoxakanner, tal int, float u str arjeqner u tpel
+x = -10
+y = -20.5
+z = "okklljkjghgj"
+print(x, y, z)  # storaketic hetw probel
+#2. haytararel a ev b  popoxakanner , tal int u float arjeqner, u tpel hetevyal toxy ` "a +  b havasar e (popoxakanneri gumary)"
+a = 30
+b = 40.5
+print("a + b = " + str(a + b))
 
-#2. haytararel a ev b  popoxakanner , tal int u float arjeqner, u tpel hetevyal toxy ` "a +  b havasar e (popoxakanneri gumary)"  
+#tpum enq data-i type-ry (popoxakani arjeqneri tesakner)
+# unenq a = 10 , aysinqn a-i data type-y int e (amboxj tiv)
+a = 10
+print(type(a))
+
+# unenq a = 0.43 , aysinqn a-i data type-y float e (kotorakayin tiv)
+a = 40.5
+print(type(a))
+
+# unenq a = "inchvor tox, str, naxadasutyu, text ev ayln" , aysinqn a-i data type-y str e (string (hayerenov tox) )
+
+a = "karen"
+print(type(a))
+
+# haytararel 3 tarber teski popoxakanner, u tpel iranc datayi type-ery
+a = 20
+x = 20.5
+y = "karen"
+print(type(a), type(x), type(y))
+
+
+# hsytararel a  u b popoxakanner tal int arjeq, bajanel irar vra , arjeqy pahel c-i mej u tpel c-i data type-y
+a = 10
+b = 2
+c = a / b
+print(type(c))
+
+
+
+
+
+
+
+
+# mi data type-ic poxel mius data type
+#orinak a = 10 in
